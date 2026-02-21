d35cd3f9fae0fbb95d59d54877cc09e1317bacf1
Alex Hal <jonathan.halimi@mail.mcgill.ca>
2026-02-21 14:31:49 -0500

    M0

---
+++ Ex.py
@@ -0,0 +1,24 @@
+OreExtraction_Level = 0
+
+
+#1. init
+
+Main() {
+    init();
+
+    # 
+    # for loop
+    # if (index < 5){
+    #}
+    # if (index < 9) {
+    #}
+
+    thresholdchar();
+
+}
+
+
+
+thresholdChar(){
+
+}
